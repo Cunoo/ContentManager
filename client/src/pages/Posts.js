7c8fb17a3172437d4bc930338270b58c2a377b7c
matus mac <ccc@gmail.com>
2025-06-06 01:48:26 +0200

    add States to post, create handleFileSelect

--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -1,9 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 const defaultImage = '/images/uploadImage.png'
-const ManagePost = () => {
+const FileUploader = () => {
     const [message, setMessage] = useState("Welcome");
-    const [selectedFile, setSelectedFile] = useState(defaultImage)
+    const [selectedFile, setSelectedFile] = useState(defaultImage);
+    const [file, setFiles] = useState([]);
+    const [isDragOver, setIsDragOver] = useState(null);
+    const [uploading, setUpLoading] = useState(null);
+    const fileInputRef = useRef(null);
+
+    const handleFileSelect = (selectedFiles) => {
+        const fileArray = Array.from(selectedFiles);
+        const newFiles = fileArray.map(file => ({
+            file,
+            id: Date.now() + Math.random() , // should be instead of math.random ID of person
+            progress: 0,
+            status: 'ready'
+        }));
+        setFiles(prev =>  [...prev, ...newFiles]);
+    }
     return (
         <div>
             <h2> Current message: {message}</h2>
@@ -21,4 +36,4 @@ const ManagePost = () => {
 
 
 
-export default ManagePost;
+export default FileUploader;
